080642a6364d3baa49e266e3d6447674030c4e8e
Luis1571 <lopezmartinluis4@gmail.com>
2023-12-27 13:53:44 +0100

    material instalado

--- circule/src/app/app.module.ts
+++ circule/src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./componentes/home/home.component";
+import { LeerComponent } from "./componentes/leer/leer.component";
+import { ConectaComponent } from './componentes/conecta/conecta/conecta.component';
+import { Conecta_ServiceService } from "./services/conecta_Service.service";
+import { HttpClientModule } from "@angular/common/http";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, HomeComponent, LeerComponent, ConectaComponent],
+  imports: [BrowserModule, AppRoutingModule,HttpClientModule, BrowserAnimationsModule,  ReactiveFormsModule,     MatFormFieldModule,
+    MatInputModule],
+  providers: [Conecta_ServiceService],
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
